b5b5d79c2762194a62924f2d328f1b5f6747ddcb
Pekka Alaluukas <pekka.alaluukas@hotmail.com>
2023-06-29 14:49:13 +0300

    henkilöiden näyttö toimii

---
+++ src/main/java/com/mycompany/myappclient/Environment.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.myappclient;
+
+/**
+ *
+ * @author pekka
+ */
+public class Environment {
+    public static String getBaseUrl(){
+        //return "http://localhost:8080";
+       return "https://myapp-anzv.onrender.com";
+    }
+}
